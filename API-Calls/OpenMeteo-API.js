c7701a76fa1949a2aac244804ebdccaec0952465
Cameron Rankin <44421748+Crankin65@users.noreply.github.com>
2023-06-08 14:07:48 -0400

    attempt to push JSON to client

--- API-Calls/OpenMeteo-API.js
+++ API-Calls/OpenMeteo-API.js
@@ -1,4 +1,5 @@
-module.exports = weatherCheck()
+const {JSONCookie} = require("cookie-parser");
+
 
 async function geoCheck(city) {
   const response = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1&language=en&format=json`, {mode: 'cors'})
@@ -23,25 +24,25 @@ async function weatherCheck(latitude,longitude){
   
     fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=temperature_2m_max&daily=temperature_2m_min&daily=precipitation_sum&daily=rain_sum&daily=windspeed_10m_max&temperature_unit=fahrenheit&windspeed_unit=kmh&precipitation_unit=mm&timeformat=iso8601&past_days=0&forecast_days=7&start_date=${date}&end_date=${date}&timezone=GMT` , {mode: 'cors'})
         .then(async function(response) {
-          console.log('first then')
+          console.log('first function then')
           let formattedData = await response.json();
           // console.log('successful to server')
           // console.log(`${format} function works`) ;
           return formattedData
         })
         .then(async function(format) {
-          console.log('second then')
+          console.log('second function then')
             console.log(format)
-            // console.log(format)
-           let weatherHash = {
-                // today: format.daily.time[0],
-                temperature_min: await format.daily.temperature_2m_min[0],
-                temperature_max: await format.daily.temperature_2m_max[0],
-                precipitation_sum: await format.daily.rain_sum[0],
-                wind_speed: await format.daily.windspeed_10m_max[0]
-            }
-
-            return weatherHash;
+            return JSON.stringify(format)
+           // let weatherHash = {
+           //      // today: format.daily.time[0],
+           //      temperature_min: await format.daily.temperature_2m_min[0],
+           //      temperature_max: await format.daily.temperature_2m_max[0],
+           //      precipitation_sum: await format.daily.rain_sum[0],
+           //      wind_speed: await format.daily.windspeed_10m_max[0]
+           //  }
+           //
+           //  return weatherHash;
           })
         .catch(function(response) {
             console.log(response)
@@ -104,4 +105,8 @@ async function getCoordinates() {
 // weatherCheck(29.76328,-95.36327)
 
 
-weatherCheck("29.76328",'-95.36327')
+// weatherCheck("29.76328",'-95.36327')
+
+
+//https://api.open-meteo.com/v1/forecast?latitude=29.76328&longitude=-95.36327&daily=temperature_2m_max&daily=temperature_2m_min&daily=precipitation_sum&daily=rain_sum&daily=windspeed_10m_max&temperature_unit=fahrenheit&windspeed_unit=kmh&precipitation_unit=mm&timeformat=iso8601&past_days=0&forecast_days=7&start_date=2023-06-08&end_date=2023-06-08&timezone=GMT
+module.exports = weatherCheck
