541694e32649e44a090a35fbd7077500fc7d90fa
Cameron Rankin <44421748+Crankin65@users.noreply.github.com>
2023-06-08 17:22:52 -0400

    Now pushing openmeteo weather api to client

--- app.js
+++ app.js
@@ -10,12 +10,14 @@ const expressAsyncHandler = require("express-async-handler");
 const weatherCheck = require("./API-Calls/OpenMeteo-API");
 
 const app = express();
+const cors = require('cors')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
