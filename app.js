9a4eef26dd0f7057053e05525307f2678b975e2d
Cameron Rankin <44421748+Crankin65@users.noreply.github.com>
2023-07-05 23:59:34 -0400

    update vercel json

--- app.js
+++ app.js
@@ -19,9 +19,6 @@ require("dotenv").config();
 const app = express();
 const cors = require('cors')
 
-
-
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
