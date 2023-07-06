1ac3fbf932991251b0fb08fb007428a19bec1c2f
Cameron Rankin <44421748+Crankin65@users.noreply.github.com>
2023-07-05 22:37:33 -0400

    moved index back to routes, and created copy of app for api folder

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const weatherCheck = require("../API-Calls/OpenMeteo/OpenMeteo-API");
+
+
+// Controllers
+const weatherController = require('../controllers/weatherController')
+const asyncHandler = require("express-async-handler");
+
+/* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+router.get('/openmeteo/:latitude/:longitude', weatherController.openMeteo);
+router.get('/openweather/:latitude/:longitude', weatherController.openWeather);
+router.get('/weatherAPI/:city', weatherController.weatherAPI);
+router.get('/get/:city', weatherController.coordinates)
+router.get('/get/weather/:city', weatherController.getAllWeather)
+
+
+
+
+
+
+module.exports = router;
