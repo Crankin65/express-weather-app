fee2681354a1299556d8e610a9b22c1581b6ea7f
Cameron Rankin <44421748+Crankin65@users.noreply.github.com>
2023-12-01 11:50:26 -0500

    Updated routes for Vercel

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const router = express.Router();
+// const router = express.Router();
+const app = express();
 const weatherCheck = require("../API-Calls/OpenMeteo/OpenMeteo-API");
 
 
@@ -8,13 +9,21 @@ const weatherController = require('../controllers/weatherController')
 const asyncHandler = require("express-async-handler");
 
 
-router.get('/openmeteo/:latitude/:longitude', weatherController.openMeteo);
-router.get('/openweather/:latitude/:longitude', weatherController.openWeather);
-router.get('/weatherAPI/:city', weatherController.weatherAPI);
-router.get('/get/:city', weatherController.coordinates)
-router.get('/get/weather/:city', weatherController.getAllWeather)
+app.get('/openmeteo/:latitude/:longitude', weatherController.openMeteo);
+app.get('/openweather/:latitude/:longitude', weatherController.openWeather);
+app.get('/weatherAPI/:city', weatherController.weatherAPI);
+app.get('/get/:city', weatherController.coordinates)
+app.get('/get/weather/:city', weatherController.getAllWeather)
+
+
+// router.get('/openmeteo/:latitude/:longitude', weatherController.openMeteo);
+// router.get('/openweather/:latitude/:longitude', weatherController.openWeather);
+// router.get('/weatherAPI/:city', weatherController.weatherAPI);
+// router.get('/get/:city', weatherController.coordinates)
+// router.get('/get/weather/:city', weatherController.getAllWeather)
 
 
 
+// module.exports = router;
+module.exports = app;
 
-module.exports = router;
