b602667baad3a51ea4db1d7c2e35190896060294
Cameron Rankin <44421748+Crankin65@users.noreply.github.com>
2023-12-01 13:33:09 -0500

    Updated index in api folder and vercel json

--- api/index.js
+++ api/index.js
@@ -1,30 +1,61 @@
-const app = require('express')();
-// const router = express.Router();
-// const app = express();
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('../routes/index');
+const expressAsyncHandler = require("express-async-handler");
 const weatherCheck = require("../API-Calls/OpenMeteo/OpenMeteo-API");
 
 
-// Controllers
-const weatherController = require('../controllers/weatherController')
-const asyncHandler = require("express-async-handler");
+// if (process.env.NODE_ENV !== "production") {
+//   require("dotenv").config();
+// }
 
-app.get('/test', weatherController.test)
+require("dotenv").config();
 
-app.get('/openmeteo/:latitude/:longitude', weatherController.openMeteo);
-app.get('/openweather/:latitude/:longitude', weatherController.openWeather);
-app.get('/weatherAPI/:city', weatherController.weatherAPI);
-app.get('/get/:city', weatherController.coordinates)
-app.get('/get/weather/:city', weatherController.getAllWeather)
+const app = express();
+const cors = require('cors')
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-// router.get('/openmeteo/:latitude/:longitude', weatherController.openMeteo);
-// router.get('/openweather/:latitude/:longitude', weatherController.openWeather);
-// router.get('/weatherAPI/:city', weatherController.weatherAPI);
-// router.get('/get/:city', weatherController.coordinates)
-// router.get('/get/weather/:city', weatherController.getAllWeather)
+app.use(logger('dev'));
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', indexRouter);
+// app.use('/users', usersRouter);
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
-// module.exports = router;
+// error handler
+app.use(function(err, req, res) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+//
+// app.listen(8000, () => {
+//   console.log(`Server is running on port 8000.`);
+// });
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Our app is running on port ${ PORT }`);
+});
+
 module.exports = app;
-
